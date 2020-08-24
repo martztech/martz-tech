e67870153baba1d18bdd7beb7a6aae70623ef171
Troy Martz <troy@offgridpro.com>
2020-08-24 14:39:26 -0500

    test

--- pages/index.old.vue
+++ pages/index.old.vue
@@ -5,17 +5,38 @@
 </template>
 
 <script>
+import BlogSection from "~/components/Sections/BlogSection";
+
 import blogsEn from "~/contents/en/blogsEn.js";
 import blogsEs from "~/contents/es/blogsEs.js";
 
 export default {
+  async asyncData({ app }) {
+    const blogs = app.i18n.locale === "en" ? blogsEn : blogsEs;
+
+    async function asyncImport(blogName) {
+      const wholeMD = await import(`~/contents/${
+        app.i18n.locale
+      }/blog/${blogName}.md`);
+      return wholeMD.attributes;
+    }
+
+    return Promise.all(blogs.map(blog => asyncImport(blog))).then(res => {
+      return {
+        blogs: res
+      };
+    });
+  },
+
+  components: { BlogSection },
+
   transition: {
     name: "slide-fade"
   },
 
   head() {
     return {
-      title: "Martz Technologies | Radical thinking in design",
+      title: this.$t("indexPageHead.title"),
       htmlAttrs: {
         lang: this.$i18n.locale
       },
