a69d90b2f2aabc12a7330f78603a6db666dc5123
Troy Martz <troy@offgridpro.com>
2020-08-27 19:21:45 -0500

    fav icons

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,75 @@
 <template>
   <div class="layout">
+    <header>
+      <section>
+        <div>
+          <div>
+            <h1>Martz Technologies</h1>
+            <h3>Radical Thinking In Systems Design</h3>
+          </div>
+        </div>
+      </section>
+
+      <nav>
+        <dl>
+          <dt>Software &amp; Application &amp; Website Design</dt>
+          <dd>
+            <nuxt-link to="/">ABC</nuxt-link>
+          </dd>
+          <dd>
+            <nuxt-link to="/">ABC</nuxt-link>
+          </dd>
+          <dd>
+            <nuxt-link to="/">ABC</nuxt-link>
+          </dd>
+          <dd>
+            <nuxt-link to="/">ABC</nuxt-link>
+          </dd>
+        </dl>
+        <dl>
+          <dt>Mechanical Prototype &amp; Product Design</dt>
+          <dd>
+            <nuxt-link to="/">ABC</nuxt-link>
+          </dd>
+          <dd>
+            <nuxt-link to="/">ABC</nuxt-link>
+          </dd>
+          <dd>
+            <nuxt-link to="/">ABC</nuxt-link>
+          </dd>
+          <dd>
+            <nuxt-link to="/">ABC</nuxt-link>
+          </dd>
+        </dl>
+        <dl>
+          <dt>Natural &amp; Regenerative Systems Design</dt>
+          <dd>
+            <nuxt-link to="/">ABC</nuxt-link>
+          </dd>
+          <dd>
+            <nuxt-link to="/">ABC</nuxt-link>
+          </dd>
+          <dd>
+            <nuxt-link to="/">ABC</nuxt-link>
+          </dd>
+          <dd>
+            <nuxt-link to="/">ABC</nuxt-link>
+          </dd>
+        </dl>
+      </nav>
+    </header>
     <nuxt class="nuxt-content"/>
     <Footer/>
   </div>
 </template>
 <script>
-  import Footer from '~/components/Sections/Footer'
+import Footer from "~/components/Sections/Footer";
 
-  export default {
-    components: {
-      Footer
-    }
+export default {
+  components: {
+    Footer
   }
+};
 </script>
 <style lang="scss">
 .layout {
